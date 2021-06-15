6330066305ce0bc6fca9782e5da57cc814eb3c19
Lewcamps <lewcamps53@gmail.com>
2021-06-14 20:53:47 -0430

    Proyecto Parte III

--- tempConvert.py
+++ tempConvert.py
@@ -9,9 +9,16 @@ var= tk.StringVar()
 
 input_label= tk.Label(root, text="Enter Temperature")
 input_entry= tk.Entry(root, textvariable=numberInput)
+result_button= tk.Button(root, text="Convert")
 
 input_label.grid(row=0)
 input_entry.grid(row=0, column=1)
+result_button.grid(row=2, columnspan=4)
+
+rLabel_1= tk.Label(root, text="Result1")
+rLabel_1.grid(row=3, columnspan=4)
+rLabel_2= tk.Label(root, text="Result2")
+rLabel_2.grid(row=4, columnspan=4)
 
 dropdownList=["Celsius ", "Fahrenheit", "Kelvin "]
 dropdown= tk.OptionMenu(root, var, *dropdownList)
