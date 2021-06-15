d3fd30917f720b2ab7169271d941bb4f70249779
Lewcamps <lewcamps53@gmail.com>
2021-06-14 22:44:50 -0430

    Proyecto Parte IV

--- tempConvert.py
+++ tempConvert.py
@@ -1,27 +1,58 @@
 import tkinter as  tk
+from functools import partial
 
 root = tk.Tk()
 root.title("Temperature Converter")
 root.iconbitmap("termometro.ico")
 
+temVal="Celsius"
+
+def store_temp(sel_temp):
+    global temVal
+    temVal=sel_temp
+
+def call_result(rl1, rl2, inputn):
+    tem= inputn.get()
+    if temVal == "Celsius":
+        f = float((float(tem) * 9/5) + 32)
+        k = float((float(tem) + 273.15))
+        rl1.config(text="%f Fahrenheit" % f)
+        rl2.config(text="%f Kelvin" % k)
+    if temVal == "Fahrenheit":
+        c = float((float(tem)- 32) * 5/9)
+        k = c + 273
+        rl1.config(text="%f Celsius" % c)
+        rl2.config(text="%f Kelvin" % k)
+    if temVal == "Kelvin":
+        c = float((float(tem)- 273.15))
+        f = float((float(tem) - 273.15) * 1.8000 + 32.00)
+        rl1.config(text="%f Celsius" % c)
+        rl2.config(text="%f Fahrenheit" % f)
+    return
+
 numberInput= tk.StringVar()
 var= tk.StringVar()
 
+
 input_label= tk.Label(root, text="Enter Temperature")
 input_entry= tk.Entry(root, textvariable=numberInput)
-result_button= tk.Button(root, text="Convert")
+
 
 input_label.grid(row=0)
 input_entry.grid(row=0, column=1)
-result_button.grid(row=2, columnspan=4)
+
 
 rLabel_1= tk.Label(root, text="Result1")
 rLabel_1.grid(row=3, columnspan=4)
 rLabel_2= tk.Label(root, text="Result2")
 rLabel_2.grid(row=4, columnspan=4)
 
-dropdownList=["Celsius ", "Fahrenheit", "Kelvin "]
-dropdown= tk.OptionMenu(root, var, *dropdownList)
+call_result=partial(call_result, rLabel_1, rLabel_2, numberInput)
+result_button= tk.Button(root, text="Convert", command=call_result)
+result_button.grid(row=1, columnspan=4)
+
+dropdownList=["Celsius", "Fahrenheit", "Kelvin"]
+dropdown= tk.OptionMenu(root, var, *dropdownList, command=store_temp )
 var.set(dropdownList[0])
 dropdown.grid(row=0, column=2)
 root.mainloop()
